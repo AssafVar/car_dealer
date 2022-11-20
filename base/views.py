29221289777a2b976bcd0e18dfc69e27147b00e6
assaf21 <assaf@ad-knight.com>
2022-11-20 15:23:42 +0200

    Adding BD models

--- base/views.py
+++ base/views.py
@@ -1,10 +1,21 @@
+from multiprocessing import context
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
 
-def home(request):
-    return  HttpResponse("home page")
+# rooms = [
+#     {'id':1, 'name':'Learn python'},
+#     {'id':2, 'name':'Frontend developers'},
+#     {'id':3, 'name':'Backend developers'},
+# ]
 
-def room(request):
-    return HttpResponse("Room")
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
+    return  render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room':room}
+    return render(request, 'base/room.html', context)
